1699e583c2a3ea6849aa27724fc26a6286d9f240
Olia <ok.00100@gmail.com>
2021-06-09 03:36:08 +0300

    async task fix

--- molp_app/views/view_anonymous_cbc.py
+++ molp_app/views/view_anonymous_cbc.py
@@ -12,78 +12,74 @@ from django.http import JsonResponse
 from django_q.tasks import async_task
 
 
-def submit_cbc(request, pk):
+def submit_cbc(problem):
     print("Task run!!!")
-    problem = Problem.objects.get(pk=pk)
-    slvr = problem.solver
 
-    if request.method == 'POST':
+    timestr, NumOfObj = parse_gurobi(problem)
 
-        timestr, NumOfObj = parse_gurobi(problem)
+    f = {}
+    weights = []
+    ystar = {}
+    models = {}
+    rho = 0.001
 
-        f = {}
-        weights = []
-        ystar = {}
-        models = {}
-        rho = 0.001
+    for obj in range(NumOfObj):
 
-        for obj in range(NumOfObj):
+        m = Model(solver_name=CBC)
+        models[obj] = m
+        obj_lp_path = settings.MEDIA_ROOT + "/problems/txt/new_problem_" + str(obj) + "_" + timestr + ".lp"
 
-            m = Model(solver_name=CBC)
-            models[obj] = m
-            obj_lp_path = settings.MEDIA_ROOT + "/problems/txt/new_problem_" + str(obj) + "_" + timestr + ".lp"
+        print(obj_lp_path)
+        m.read(obj_lp_path)
+        print('model has {} vars, {} constraints and {} nzs'.format(m.num_cols, m.num_rows, m.num_nz))
 
-            print(obj_lp_path)
-            m.read(obj_lp_path)
-            print('model has {} vars, {} constraints and {} nzs'.format(m.num_cols, m.num_rows, m.num_nz))
+        m.max_gap = 0.25
+        status = m.optimize(max_seconds=100)
 
-            m.max_gap = 0.25
-            status = m.optimize(max_seconds=10)
+        if status == OptimizationStatus.OPTIMAL:
+            print('optimal solution cost {} found'.format(m.objective_value))
+            ystar[obj] = m.objective_value
+        elif status == OptimizationStatus.FEASIBLE:
+            print('sol.cost {} found, best possible: {}'.format(m.objective_value, m.objective_bound))
+            ystar[obj] = m.objective_value
+        elif status == OptimizationStatus.NO_SOLUTION_FOUND:
+            print('no feasible solution found, lower bound is: {}'.format(m.objective_bound))
+            ystar[obj] = m.objective_bound
 
-            if status == OptimizationStatus.OPTIMAL:
-                print('optimal solution cost {} found'.format(m.objective_value))
-                ystar[obj] = m.objective_value
-            elif status == OptimizationStatus.FEASIBLE:
-                print('sol.cost {} found, best possible: {}'.format(m.objective_value, m.objective_bound))
-                ystar[obj] = m.objective_value
-            elif status == OptimizationStatus.NO_SOLUTION_FOUND:
-                print('no feasible solution found, lower bound is: {}'.format(m.objective_bound))
-                ystar[obj] = m.objective_bound
+        os.remove(obj_lp_path)
 
-            os.remove(obj_lp_path)
+    # chebyshev scalarization
+    ch = Model(sense=MINIMIZE, solver_name=CBC)
+    m = models[0]
+    print(ystar)
+    for v in m.vars:
+        ch.add_var(name=v.name, var_type=v.var_type)
 
-        # chebyshev scalarization
-        ch = Model(sense=MINIMIZE, solver_name=CBC)
-        m = models[0]
-        print(ystar)
-        for v in m.vars:
-            ch.add_var(name=v.name, var_type=v.var_type)
+    for c in m.constrs:
+        ch.add_constr(c.expr, c.name)
 
-        for c in m.constrs:
-            ch.add_constr(c.expr, c.name)
+    ch.add_var(name='s', var_type=CONTINUOUS)
+    ch.objective = ch.vars['s']
 
-        ch.add_var(name='s', var_type=CONTINUOUS)
-        ch.objective = ch.vars['s']
+    for i in range(NumOfObj):
+        ch.add_var(name='f{}'.format(i + 1), var_type=CONTINUOUS)
+        f[i] = ch.vars['f{}'.format(i + 1)]
+        weights = np.full(NumOfObj, 1 / NumOfObj)
 
-        for i in range(NumOfObj):
-            ch.add_var(name='f{}'.format(i + 1), var_type=CONTINUOUS)
-            f[i] = ch.vars['f{}'.format(i + 1)]
-            weights = np.full(NumOfObj, 1 / NumOfObj)
+    for i in range(NumOfObj):
+        ch.add_constr(
+            weights[i] * (ystar[i] - f[i]) + xsum(rho * (ystar[i] - f[i]) for i in range(NumOfObj)) <= ch.vars['s'],
+            'sum{}'.format(i + 1))
 
-        for i in range(NumOfObj):
-            ch.add_constr(
-                weights[i] * (ystar[i] - f[i]) + xsum(rho * (ystar[i] - f[i]) for i in range(NumOfObj)) <= ch.vars['s'],
-                'sum{}'.format(i + 1))
+    for obj in range(NumOfObj):
 
-        for obj in range(NumOfObj):
+        m = models[obj]
+        o = m.objective
+        ch.add_constr(f[obj] - o == 0, 'f_constr_' + str(obj))
 
-            m = models[obj]
-            o = m.objective
-            ch.add_constr(f[obj] - o == 0, 'f_constr_' + str(obj))
+    # ch.write(settings.MEDIA_ROOT + "/problems/chebyshev/chebyshev_" + timestr + ".lp")
 
-        # ch.write(settings.MEDIA_ROOT + "/problems/chebyshev/chebyshev_" + timestr + ".lp")
-
-        save_files('chebknap', '/problems/chebyshev/', 'lp', 'chebyshev', problem, ch)
+    save_files('chebknap', '/problems/chebyshev/', 'lp', 'chebyshev', problem, ch)
 
     #     context = get_context()
     #     context.update({'solver': slvr})
@@ -94,12 +90,13 @@ def submit_cbc(request, pk):
 def submit_cbc_problem(request, pk):
     problem = Problem.objects.get(pk=pk)
     slvr = problem.solver
-    # json_payload = {"message": "chebyshev scalarization obtained"}
 
-    async_task(submit_cbc(request, pk))
+    if request.method == 'POST':
+
+        async_task(submit_cbc, problem)
 
     # return JsonResponse(json_payload)
     context = get_context()
     context.update({'solver': slvr})
-    return render(request, 'user_problems.html', context)
+    return render(request, 'problem_list.html', context)
 
