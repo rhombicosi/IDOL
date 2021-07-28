3f749d75d0037277afc2ef40bf451f9575f9cfee
Olia <ok.00100@gmail.com>
2021-07-28 19:30:05 +0300

    celery channels

--- molp_project/settings.py
+++ molp_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
