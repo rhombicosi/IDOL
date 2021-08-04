c1b4717080d69fce43df5372b7759ee3f38856bf
Olia <ok.00100@gmail.com>
2021-08-04 11:08:32 +0300

    celery beat worker django channels

--- molp_project/settings.py
+++ molp_project/settings.py
@@ -94,26 +94,26 @@ ASGI_APPLICATION = 'molp_project.asgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'molp',
+        'USER': 'postgres',
+        'PASSWORD': 'wipeOUT',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'molp',
 #         'USER': 'postgres',
 #         'PASSWORD': config('POSTGRE_PASSWORD'),
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'HOST': 'localhost'
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'molp',
-        'USER': 'postgres',
-        'PASSWORD': config('POSTGRE_PASSWORD'),
-        'HOST': 'localhost'
-    }
-}
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
