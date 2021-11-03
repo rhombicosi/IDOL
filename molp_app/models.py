b0be715903397c4d8b955159ba5e175d46b7a2d9
rhombicosi <ok.00100@gmail.com>
2021-11-04 01:54:28 +0300

    file name + params links

--- molp_app/models.py
+++ molp_app/models.py
@@ -18,7 +18,7 @@ class Problem(models.Model):
         super().delete(*args, **kwargs)
 
     def file_name(self):
-        return self.xml.name.split('/')[2]
+        return self.xml.name.split('/')[2][:-40]
 
 
 class ProblemParameters(models.Model):
@@ -66,7 +66,7 @@ class UserProblem(models.Model):
         super().delete(*args, **kwargs)
 
     def file_name(self):
-        return self.xml.name.split('/')[2]
+        return self.xml.name.split('/')[2][:40]
 
 
 class UserProblemParameters(models.Model):
