9dc1c3cd82cf9bd97b0828f1af67129ccd756fee
rhombicosi <ok.00100@gmail.com>
2021-11-04 04:03:06 +0300

    pending and file name fix

--- molp_app/utilities/scalarization.py
+++ molp_app/utilities/scalarization.py
@@ -315,8 +315,12 @@ def get_user_tasks_info():
     tasks_info = []
     for p in problems:
         if p.task_id:
-            p.task_status = AsyncResult(p.task_id).status
-            p.save()
+            if not p.task_status:
+                p.task_status = AsyncResult(p.task_id).status
+                p.save()
+            if p.task_status == 'PENDING':
+                p.task_status = 'Press the "Make Chebyshev" button'
+                p.save()
             tasks_info.append({'task_id': p.task_id, 'task_status': p.task_status, 'problem_pk': p.id})
 
     async_to_sync(channel_layer.group_send)('user_scalarizations', {'type': 'send_user_scalarizations', 'text': tasks_info})
